6916097b5889b219d8d74ed1afb0d05cfe4d52f7
arvind-verz <arvind-verz@users.noreply.github.com>
2018-09-25 12:19:25 +0530

    updates

---
+++ application/views/backend/include/footer.php
@@ -0,0 +1,42 @@
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js'); ?>"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo base_url('assets/plugins/jquery-ui/jquery-ui.min.js'); ?>"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url('assets/plugins/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+<!-- Morris.js charts -->
+<script src="<?php echo base_url('assets/plugins/raphael/raphael.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugins/morris.js/morris.min.js'); ?>"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url('assets/plugins/jquery-sparkline/dist/jquery.sparkline.min.js'); ?>"></script>
+<!-- jvectormap -->
+<script src="<?php echo base_url('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js'); ?>"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url('assets/plugins/jquery-knob/dist/jquery.knob.min.js'); ?>"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url('assets/plugins/moment/min/moment.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap-daterangepicker/daterangepicker.js'); ?>"></script>
+<!-- datepicker -->
+<script src="<?php echo base_url('assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js'); ?>"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="<?php echo base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js'); ?>"></script>
+<!-- Slimscroll -->
+<script src="<?php echo base_url('assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js'); ?>"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url('assets/plugins/fastclick/lib/fastclick.js'); ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('assets/dist/js/adminlte.min.js'); ?>"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?php echo base_url('assets/dist/js/pages/dashboard.js'); ?>"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url('assets/dist/js/demo.js'); ?>"></script>
+</body>
+</html>
---
+++ application/views/backend/include/sidebar.php
@@ -0,0 +1,32 @@
+<aside class="main-sidebar">
+<!-- sidebar: style can be found in sidebar.less -->
+<section class="sidebar">
+<!-- Sidebar user panel -->
+<div class="user-panel">
+<!-- /.search form -->
+<!-- sidebar menu: : style can be found in sidebar.less -->
+<ul class="sidebar-menu" data-widget="tree">
+<li class="active">
+<a href="#">
+<i class="fa fa-dashboard"></i> <span>Dashboard</span>
+</a>
+</li>
+<li class="">
+<a href="#">
+<i class="fa fa-dashboard"></i> <span>Subject</span>
+</a>
+</li>
+<li class="">
+<a href="#">
+<i class="fa fa-dashboard"></i> <span>Tutor</span>
+</a>
+</li>
+<li class="">
+<a href="<?php echo site_url(''); ?>">
+<i class="fa fa-dashboard"></i> <span>Class</span>
+</a>
+</li>
+</ul>
+</section>
+<!-- /.sidebar -->
+</aside>
