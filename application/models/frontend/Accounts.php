445caae8daecaffc721a2ae02901432a904f7f2b
arvind-verz <arvind-verz@users.noreply.github.com>
2018-11-05 15:45:12 +0530

    updates

--- application/models/frontend/Accounts.php
+++ application/models/frontend/Accounts.php
@@ -16,24 +16,43 @@ class Accounts extends CI_Model
         $password    = isset($_POST['password']) ? $_POST['password'] : '';
         $remember_me = isset($_POST['remember_me']) ? $_POST['remember_me'] : 0;
 
-        $cookie = [
-            'name'   => 'remember_me_token',
-            'domain' => '',
-            'path'   => '/',
-        ];
-        delete_cookie($cookie);
+        
 
         $query = $this->db->get_where(DB_STUDENT, ['email' => $email]);
         if ($query->num_rows() > 0) {
             $result = $query->row();
             if (password_verify($password, $result->password)) {
                 if ($remember_me) {
-                    $cookie = [
-                        'name'   => 'remember_me_token',
-                        'value'  => $remember_me,
+                    $cookie_email = [
+                        'name'   => '_cemail',
+                        'value'  => $email,
+                        'domain' => '',
+                        'path'   => '/',
+                        'expire'    =>  36500,
+                    ];
+                    set_cookie($cookie_email);
+                    $cookie_password = [
+                        'name'   => '_cpassword',
+                        'value'  => $password,
+                        'domain' => '',
+                        'path'   => '/',
+                        'expire'    =>  36500,
+                    ];
+                    set_cookie($cookie_password);
+                }
+                else {
+                    $cookie_email = [
+                        'name'   => '_cemail',
                         'domain' => '',
                         'path'   => '/',
                     ];
+                    delete_cookie($cookie_email);
+                    $cookie_password = [
+                        'name'   => '_cpassword',
+                        'domain' => '',
+                        'path'   => '/',
+                    ];
+                    delete_cookie($cookie_password);
                 }
 
                 $student_data = array(
