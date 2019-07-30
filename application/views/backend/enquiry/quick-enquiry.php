7baa7d1f448066d3acff05eda2d6c55ba060685e
Arvind Purohit <arvind.verz@gmail.com>
2019-07-30 21:42:07 +0530

    table updates

--- application/views/backend/enquiry/quick-enquiry.php
+++ application/views/backend/enquiry/quick-enquiry.php
@@ -1,52 +1,67 @@
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-        <?php print_r($page_title);?>
-        </h1>
-        <?php print_r($breadcrumbs);?>
-    </section>
-    <?php $this->load->view('backend/include/messages')?>
-    <!-- Main content -->
-    <section class="content">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="box">
-                    <div class="box-body">
-                        <table class="table table-striped table-bordered text-center" id="datatable" style="width:100%">
-                            <thead>
-								<tr>
-									<th>Name</th>
-									<th>Email</th>
-									<th>Phone No.</th>
-									<th>Message</th>
-									<th>Created At</th>
-								</tr>
-                            </thead>
-                            <tbody>
-								<?php
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+		<h1>
+			<?php print_r($page_title);?>
+		</h1>
+		<?php print_r($breadcrumbs);?>
+	</section>
+	<?php $this->load->view('backend/include/messages')?>
+	<!-- Main content -->
+	<section class="content">
+		<!-- Small boxes (Stat box) -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="box">
+					<div class="box-body ">
+						<div class="table-responsive">
+							<table class="table table-striped table-bordered datatable" id="" style="width:100%">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>Email</th>
+										<th>Phone No.</th>
+										<th>Message</th>
+										<th>Created At</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
 								if($quick_enquiry)
 								{
 									foreach($quick_enquiry as $enquiry)
 									{
 								?>
-								<tr>
-									<td><?php echo isset($enquiry->name) ? $enquiry->name : '-'; ?></td>
-									<td><?php echo isset($enquiry->email) ? $enquiry->email : '-'; ?></td>
-									<td><?php echo isset($enquiry->phone_no) ? $enquiry->phone_no : '-'; ?></td>
-									<td><?php echo isset($enquiry->message) ? $enquiry->message : '-'; ?></td>
-									<td><?php echo isset($enquiry->create_date) ? date('Y-m-d h:i A', strtotime($enquiry->create_date)) : '-'; ?></td>
-								</tr>
-								<?php
+									<tr>
+										<td><?php echo isset($enquiry->name) ? $enquiry->name : '-'; ?></td>
+										<td><?php echo isset($enquiry->email) ? $enquiry->email : '-'; ?></td>
+										<td><?php echo isset($enquiry->phone_no) ? $enquiry->phone_no : '-'; ?></td>
+										<td><?php echo isset($enquiry->message) ? $enquiry->message : '-'; ?></td>
+										<td
+											data-order="<?php echo isset($enquiry->create_date) ? $enquiry->create_date : ''; ?>">
+											<?php echo isset($enquiry->create_date) ? date('Y-m-d h:i A', strtotime($enquiry->create_date)) : '-'; ?>
+										</td>
+									</tr>
+									<?php
 									}
 								}
 								?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
+<script>
+	$(document).ready(function () {
+		$(".datatable").DataTable({
+			"order": [
+				[4, "desc"]
+			]
+		});
+	});
+
+</script>
